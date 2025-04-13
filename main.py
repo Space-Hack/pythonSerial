bf202d712556da4fb0740c137e8aa076e37a1271
lukasdiebold <lukas.diebold@gmail.com>
2025-04-13 13:02:39 +0200

    Remove debug print

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import time
 import threading
 from websockets.sync.client import connect, ClientConnection
 
-SERIAL_PORT = '/dev/ttyACM1'
+# SERIAL_PORT = '/dev/ttyACM1'
+SERIAL_PORT = '/dev/tty.usbmodem2103'
 BAUD_RATE = 115200
 WEBSOCKET_URI = "ws://localhost:8765"
 
@@ -33,32 +34,41 @@ def read_from_websocket(ser: serial.Serial, websocket: ClientConnection):
         time.sleep(0.01)
 
 
-try:
-    with connect(WEBSOCKET_URI) as websocket:
-        name = input("What's your name? ")
-        websocket.send(name)
-        print(f">>> {name}")
-        greeting = websocket.recv()
-        print(f"<<< {greeting}")
+def start_socket():
+    try:
+        with connect(WEBSOCKET_URI) as websocket:
+            name = input("What's your name? ")
+            websocket.send(name)
+            print(f">>> {name}")
+            greeting = websocket.recv()
+            print(f"<<< {greeting}")
 
-        ser = serial.Serial(SERIAL_PORT, BAUD_RATE, timeout=1)
-        print(f"Connected to {ser.name}")
+            ser = serial.Serial(SERIAL_PORT, BAUD_RATE, timeout=1)
+            print(f"Connected to {ser.name}")
 
-        # Start serial to websocket thread
-        serial_thread = threading.Thread(
-            target=read_from_serial, args=(ser, websocket), daemon=True)
-        serial_thread.start()
+            # Start serial to websocket thread
+            serial_thread = threading.Thread(
+                target=read_from_serial, args=(ser, websocket), daemon=True)
+            serial_thread.start()
 
-        # Main thread handles websocket to serial
-        read_from_websocket(ser, websocket)
+            # Main thread handles websocket to serial
+            read_from_websocket(ser, websocket)
 
-except serial.SerialException as e:
-    print(f"Serial error: {e}")
-except KeyboardInterrupt:
-    print("Program terminated by user.")
-except Exception as e:
-    print(f"General error: {e}")
-finally:
-    if 'ser' in locals() and ser.is_open:
-        ser.close()
-        print("Serial port closed.")
+    except serial.SerialException as e:
+        print(f"Serial error: {e}")
+    except KeyboardInterrupt:
+        print("Program terminated by user.")
+    except Exception as e:
+        print(f"General error: {e}")
+    finally:
+        if 'ser' in locals() and ser.is_open:
+            ser.close()
+            print("Serial port closed.")
+
+if __name__ == "__main__":
+    ser = serial.Serial(SERIAL_PORT, BAUD_RATE, timeout=1)
+    # ser.write(("FORWARD:100.0" + '\n').encode('utf-8'))
+    # time.sleep(2)
+    # ser.write(("BACKWARD:10.0" + '\n').encode('utf-8'))
+    # ser.write(("TURN:10.0" + '\n').encode('utf-8'))
+    # ser.write(("STOP" + '\n').rencode('utf-8'))
